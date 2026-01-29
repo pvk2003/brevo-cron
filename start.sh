473474fc89503c8ab38fef025e8f9249fd87cb8a
PhamViKhang <21057711.khang@student.iuh.edu.vn>
2026-01-29 22:56:20 +0700

    CAPNHAT

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 set -e
+
 echo "$GSPREAD_SERVICE_ACCOUNT_JSON" > service_account.json
-python -m flask --app app run --host 0.0.0.0 --port $PORT
+
+python -m flask --app app run --host 0.0.0.0 --port 8080
